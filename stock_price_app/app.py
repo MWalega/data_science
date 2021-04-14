1f178e46692e8d933a00ce46436fb2df3f716c13
Maciek Walega <mwaleg.sa@gmail.com>
2021-04-14 06:02:58 +0200

    Initial commit

---
+++ stock_price_app/app.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import streamlit as st
+import yfinance as yf
+
+
+st.write("""
+# Simple Stock Price App
+
+This app shows stock closing price and volume of Google!
+
+""")
+
+# define the ticker symbol
+ticker_symbol = 'GOOGL'
+
+# get data on this ticker
+ticker_data = yf.Ticker(ticker_symbol)
+
+# get the historical prices for this ticker
+ticker_df = ticker_data.history(period='1d', start='2011-04-13', end='2021-04-13')
+# Open / High / Low / Close / Volume / Dividends / Stock Splits
+
+st.line_chart(ticker_df.Close)
+st.line_chart(ticker_df.Volume)
